36cf703d8f6e591587aebd7ba72fd668c7cc3a1e
hsisodi <hridesh.sisodia@daimler.com>
2021-03-01 18:50:24 +0530

    router added

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Controller from './screens/Controller';
 
 ReactDOM.render(
-    <div>
-        Food Ordering App
-    </div>,
+    <Controller />,
     document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
